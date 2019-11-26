7c457b4291d8d5d1b21122ba08698eaa7b1c9833
tancredi89 <56297690+tancredi89@users.noreply.github.com>
2019-11-26 02:26:33 +0100

    Complete

---
+++ gui.py
@@ -0,0 +1,53 @@
+from tkinter import *
+from main import passes
+
+
+window = Tk()
+window.title("Wimpy Searcher")
+window.configure(background = "dark grey")
+window.geometry("700x500+500+300")
+
+
+def click():
+    metop = metopEntry.get() # collect input from textbox
+    orbit = orbitEntry.get() 
+    output.delete(0.0,END)
+    number = int(metop[-1])
+    counter = float(5.0)
+    for p in passes[number-1]:
+        if orbit in p:
+            if 'Az' in p:
+                counter = 1.0
+            elif 'AOS5' in p:
+                counter = 3.0
+            elif 'LOS5' in p:
+                counter = 4.0
+            elif 'AOS' in p:
+                counter = 2.0
+            elif 'LOS' in p:
+                counter = 5.0
+            textOutput = str(p)+" "+str(passes[number-1][p]+'\n')
+            output.insert(counter,textOutput)
+
+
+# Title
+Label(window, text="Wimpy Searcher", bg="dark grey", fg="black", font="none 20 bold").grid(row=0, column=0,sticky=W)
+
+# User Inputs
+Label(window, text="Metop [M0x]: ", bg="dark grey", fg="black", font="none 14") .grid(row=1, column=0, sticky=W)
+metopEntry = Entry(window, width=15, bg="white")
+metopEntry .grid(row=1, column=0, sticky=E)
+
+Label(window, text="Orbit Number: ", bg="dark grey", fg="black", font="none 14") .grid(row=2, column=0, sticky=W)
+orbitEntry = Entry(window, width=15, bg="white")
+orbitEntry .grid(row=2, column=0, sticky=E)
+
+
+# Button
+Button(window, text = "Search", width = 6, command = click,bg="white").grid(row=2, column=100, sticky=E)
+
+# Result section
+Label (window, text="\nResult: ", bg="dark grey", fg="black", font="none 14") . grid(row= 6, column = 0,sticky=W)
+output = Text(window, width=75,height=6, wrap=WORD,background="white")
+output.grid(row=7, column=0,columnspan=2,sticky=W)
+window.mainloop()
--- main.py
+++ main.py
@@ -1,6 +1,58 @@
 import pandas as pd 
-import re
+from pathlib import Path 
 
-with open('wimpy_m01.txt') as file:
-    content = file.read()
-    print(content)
+
+def split(word): 
+    return list(word) 
+
+lines = []
+
+passes = [{},{},{}]
+
+for i in range(1,4):
+    wimpy_name = 'wimpy_m0{0}'.format(i)
+
+    with open("""P:\groups\OPS\EPS System Ops\Wimpys\\{0}""".format(wimpy_name),'r') as file:
+        content = file.readlines()
+        file.close()
+        for c in content:
+            lines.append(c.split('\n'))
+
+    for x,l in enumerate(lines):
+        if l == ['']:
+            lines.remove(l)
+        elif '-' not in l[0]:
+            lines.remove(l)
+        elif l == [' --------------- - - -------  ---------  ---------  ---------  ---------  ---------  ---------  ---------']:
+            lines.remove(l)
+        elif 'Wimpy' in l[0]:
+            lines.remove(l)
+        else:
+            if len(lines[x]) > 1:
+                del lines[x][1] 
+            StringLine = split(str(l))
+            orbit = ''
+            for o in range(23,29):
+                orbit = orbit+StringLine[o]
+            
+            time = ''
+            for t in range(6,19):
+                time = time + StringLine[t]
+            
+            Az = ''
+            for a in range(32,35):
+                Az = Az + StringLine[a]
+
+            if StringLine[39] == 'a' and (StringLine[40] == '1' or StringLine[40] == '2'):
+                Az = int(Az)
+                if Az > 260:
+                    Az = Az - 360
+                passes[i-1]["M0{1}-{0}-Az".format(orbit,i)] = str(Az)
+                passes[i-1]["M0{1}-{0}-AOS".format(orbit,i)] = time
+            elif StringLine[39] == 'A':
+                passes[i-1]["M0{1}-{0}-AOS5".format(orbit,i)] = time
+            elif StringLine[39] == 'L':
+                passes[i-1]["M0{1}-{0}-LOS5".format(orbit,i)] = time
+            elif StringLine[39] == 'l':
+                passes[i-1]["M0{1}-{0}-LOS".format(orbit,i)] = time
+
